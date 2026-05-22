7aaa8080977c1839ac67ab0d9eceb21741b76c2c
Ichiky_Otsu <bricedelcroix20@gmail.com>
2026-05-22 22:22:19 +0200

    feat : dockerfile and remove the db

--- src/bot.py
+++ src/bot.py
@@ -23,7 +23,7 @@ async def _main_async():
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT", 21003)),
+        port=int(os.getenv("DB_PORT", 3306)),
         charset="utf8mb4",
         autocommit=True
     )
